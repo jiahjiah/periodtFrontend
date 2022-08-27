84323617778e64832d444558d56ca7dd66ac70f6
Yi Jia Huang <yijh1705@gmail.com>
2022-08-27 10:11:58 -0400

    Initial Commit

---
+++ periodt/periodtApp.swift
@@ -0,0 +1,17 @@
+//
+//  periodtApp.swift
+//  periodt
+//
+//  Created by HDSB on 2022-08-27.
+//
+
+import SwiftUI
+
+@main
+struct periodtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
