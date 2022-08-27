0e6e3d1f072e3914c73edc11b618d72ffeb20317
Yi Jia Huang <yijh1705@gmail.com>
2022-08-27 13:52:02 -0400

    added function where you can choose the date you want with an animation

--- HomeView.swift
+++ HomeView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct HomeView: View {
     @StateObject var weekModel: WeekViewModel = WeekViewModel()
+    @Namespace var animation
     var body: some View {
         ScrollView {
             VStack {
@@ -31,42 +32,56 @@ struct HomeView: View {
                 
                 Image("heart 1")
                 
+                ScrollView(.horizontal, showsIndicators: false) {
+                    HStack(spacing: 10) {
+                        ForEach(weekModel.currentWeek,id: \.self) { day in
+                            //EEE will return day as MON, TUE, ...
+                            
+                            VStack(spacing: 10) {
+                                
+                                Text(weekModel.extractDate(date: day, format: "dd"))
+                                    .font(.system(size: 15))
+                                    .fontWeight(.semibold)
+
+                                Text(weekModel.extractDate(date: day, format: "EEE"))
+                                    .font(.system(size: 14))
+                                
+                                Circle()
+                                    .fill(.white)
+                                    .frame(width: 8, height: 8)
+                                    .opacity(weekModel.isToday(date: day) ? 1 : 0)
+                            } //MARK: Foreground Stule
+                            .foregroundColor(weekModel.isToday(date: day) ? .white : Color.myPink)
+                            //MARK: Capsule Shape
+                            .frame(width: 45, height: 90)
+                            .background(
+                                ZStack {
+                                    //MARK: Matched Geometry Effect
+                                    if weekModel.isToday(date: day) {
+                                        Capsule()
+                                            .fill(Color.myPink)
+                                            .matchedGeometryEffect(id: "CURRENTDAY", in: animation)
+                                    }
+                                }
+                            )
+                            .contentShape(Capsule())
+                            .onTapGesture {
+                                //updating current day
+                                withAnimation {
+                                    weekModel.currentDate = day
+                                }
+                            }
+                        }
+                    }
+                    .padding(.horizontal)
+                }
+                .padding(.bottom, 10)
+                
             
             }
             .background(Color.background)
             
-            ScrollView(.horizontal) {
-                HStack(spacing: 10) {
-                    ForEach(weekModel.currentWeek,id: \.self) { day in
-                        //EEE will return day as MON, TUE, ...
                         
-                        VStack(spacing: 10) {
-                            
-                            Text(weekModel.extractDate(date: day, format: "dd"))
-                                .font(.system(size: 15))
-                                .fontWeight(.semibold)
-
-                            Text(weekModel.extractDate(date: day, format: "EEE"))
-                                .font(.system(size: 14))
-                            
-                            Circle()
-                                .fill(.white)
-                                .frame(width: 8, height: 8)
-                        }
-                        .foregroundColor(.white)
-                        //MARK: Capsule Shape
-                        .frame(width: 45, height: 90)
-                        .background(
-                            ZStack {
-                                Capsule()
-                                    .fill(Color.myPink)
-                            }
-                        )
-                    }
-                }
-                .padding(.horizontal)
-            }
-            
             
         }
     }
